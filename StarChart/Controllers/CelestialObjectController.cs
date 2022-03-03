65e23eae5c6582c58f53a3bdd6d1697165b90383
David Okeke <davidokeke.c@gmail.com>
2022-03-03 23:16:13 +0100

    Update CelestialObjects Controller

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
-using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -23,63 +19,73 @@ namespace StarChart.Controllers
         public IActionResult GetById(int id)
         {
             var celestialObject = _context.CelestialObjects.Where(x => x.Id == id).FirstOrDefault();
-            celestialObject.Satellites = (from celestials in _context.CelestialObjects
-                                          where celestials.Id == id
-                                          select celestials).ToList();
+
             if (celestialObject == null)
-            {
                 return NotFound();
-            }
+
+            celestialObject.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == id).ToList();
             return Ok(celestialObject);
         }
 
         [HttpGet("{name}", Name = "GetByName")]
         public IActionResult GetByName(string name)
         {
-            var celestialObject = _context.CelestialObjects.Where(x => x.Name == name).FirstOrDefault();
-            var celestialObjectId = celestialObject.Id;
-            celestialObject.Satellites = (from celestials in _context.CelestialObjects
-                                          where celestials.Id == celestialObjectId
-                                          select celestials).ToList();
-            if (celestialObject == null)
+            var celestialObjects = _context.CelestialObjects.Where(x => x.Name == name).ToList();
+
+            if (celestialObjects.Count == 0)
             {
                 return NotFound();
             }
-            return Ok(celestialObject);
+
+            foreach (var celestialObject in celestialObjects)
+            {
+                celestialObject.Satellites = celestialObjects.Where(x => x.Id == celestialObject.Id).ToList();
+            }
+            _context.SaveChanges();
+            return Ok(celestialObjects);
         }
 
         [HttpGet]
         public IActionResult GetAll()
         {
-            // account for streaming and non streaming deferred execution
-            var celestialObjectGroups = (from celestialObj in _context.CelestialObjects
-                                         orderby celestialObj.Id
-                                         group celestialObj by celestialObj.Id
-                                         into idGroups
-                                         select idGroups);
-
             var celestialObjects = _context.CelestialObjects.ToList();
 
-            // Linq code for doing this?
-            foreach (var grp in celestialObjectGroups)
+            foreach (var celestialObject in celestialObjects)
             {
-                foreach (var obj in celestialObjects)
-                {
-                    if (obj.Id == grp.Key)
-                    {
-                        var groupObjectName = _context.CelestialObjects.Find(obj.Id).Name;
-                        if (obj.Name != groupObjectName)
-                        {
-                            obj.Satellites.Add((CelestialObject)celestialObjects.Where(x => x.Id == grp.Key));
-                        }
-                    }
-                }
-            }
+                var celestialObjectId = celestialObject.Id;
+                celestialObject.Satellites = celestialObjects.Where(x => x.OrbitedObjectId == celestialObject.Id).ToList();
+            };
 
-            if (celestialObjects == null)
-            {
-                return NotFound();
-            }
+            //// account for streaming and non streaming deferred execution
+            //var celestialObjectGroups = (from celestialObj in _context.CelestialObjects
+            //                             orderby celestialObj.Id
+            //                             group celestialObj by celestialObj.Id
+            //                             into idGroups
+            //                             select idGroups);
+
+            //var celestialObjects = _context.CelestialObjects.ToList();
+
+            //// Linq code for doing this?
+            //foreach (var grp in celestialObjectGroups)
+            //{
+            //    foreach (var obj in celestialObjects)
+            //    {
+            //        if (obj.Id == grp.Key)
+            //        {
+            //            var groupObjectName = _context.CelestialObjects.Find(obj.Id).Name;
+            //            if (obj.Name != groupObjectName)
+            //            {
+            //                obj.Satellites.Add((CelestialObject)celestialObjects.Where(x => x.Id == grp.Key));
+            //            }
+            //        }
+            //    }
+            //}
+
+            //if (celestialObjects == null)
+            //{
+            //    return NotFound();
+            //}
+            //_context.SaveChanges();
             return Ok(celestialObjects);
         }
     }
